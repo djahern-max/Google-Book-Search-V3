01a2a8b116c75f9eb3960c1623faa5b6be62f522
Dane Ahern <danielaherniv@gmail.com>
2019-11-18 14:42:30 -0500

    update

--- client/src/BookCard.js
+++ client/src/BookCard.js
@@ -1,21 +1,24 @@
 import React from 'react';
 import App from './App';
 
-const BookCard = (props) => {
-    return (
-
-        <div className="card-container">
-
-            <img src={props.image} alt="" />
-            <div className="desc">
-                <h2>{props.title}</h2>
-                <p>Author: {props.author}</p>
-                <p>Published: {props.published === '0000' ? 'Not Available' : props.published.substring(0, 4)}</p>
-            </div>
-        </div>
-
-
-    )
-}
+const BookCard = props => {
+  return (
+    <div className='card-container'>
+      <img src={props.image} alt='' />
+      <div className='desc'>
+        <h2>{props.title}</h2>
+        <h2>AUTHOR: {props.author}</h2>
+        <h2>
+          PUBLISHED:{' '}
+          {props.published === '0000'
+            ? 'Not Available'
+            : props.published.substring(0, 4)}
+        </h2>
+        <h2>LINK: {props.link}</h2>
+        <h2>DESCRIPTION: {props.description}</h2>
+      </div>
+    </div>
+  );
+};
 
 export default BookCard;
